05443b146bc92a5d229cd4354fb0bdf350a5ac3f
Alex <2222@2222.com>
2020-06-17 20:35:57 +0800

    恢复误删

--- IDEA_Workspace/FlinkSZ1128/src/main/scala/com/atgp/day3/redis/SinkToES.scala
+++ IDEA_Workspace/FlinkSZ1128/src/main/scala/com/atgp/day3/redis/SinkToES.scala
@@ -1,67 +1,56 @@
 package com.atgp.day3.redis
 
 import com.atgp.day2.{SensorReading, SensorSource}
+import java.util
+import org.apache.flink.api.common.functions.RuntimeContext
 import org.apache.flink.streaming.api.scala._
-import org.apache.flink.table.api.scala._
-import org.apache.flink.api.scala._
-import org.apache.flink.table.api.{DataTypes, EnvironmentSettings, Table}
-import org.apache.flink.table.descriptors.{Csv, FileSystem, Schema}
+import org.apache.flink.streaming.connectors.elasticsearch.{ElasticsearchSinkFunction, RequestIndexer}
+import org.apache.flink.streaming.connectors.elasticsearch6.ElasticsearchSink
+import org.apache.http.HttpHost
+import org.elasticsearch.client.Requests
 
-object FlinkTableExample {
+object SinkToES {
     def main(args: Array[String]): Unit = {
-        // 创建执行环境
         val env = StreamExecutionEnvironment.getExecutionEnvironment
         env.setParallelism(1)
 
-        // 创建表环境, 使用blink planner
+        val stream = env
+                .addSource(new SensorSource)
 
-        val settings = EnvironmentSettings
-                .newInstance()
-                .useBlinkPlanner()
-                .inStreamingMode()
-                .build()
+        // es的主机和端口
+        val httpHosts = new util.ArrayList[HttpHost]()
+        httpHosts.add(new HttpHost("hadoop102", 9200))
 
-        val tableEnv = StreamTableEnvironment.create(env, settings)
+        // 定义了如何将数据写入到es中去
+        val esSinkBuilder = new ElasticsearchSink.Builder[SensorReading](
+            httpHosts, // es的主机名
+            // 匿名类，定义如何将数据写入到es中
+            new ElasticsearchSinkFunction[SensorReading] {
+                override def process(t: SensorReading,
+                                     runtimeContext: RuntimeContext,
+                                     requestIndexer: RequestIndexer): Unit = {
+                    // 哈希表的key为string，value为string
+                    val json = new util.HashMap[String, String]()
+                    json.put("data", t.toString)
+                    // 构建一个写入es的请求
+                    val indexRequest = Requests
+                            .indexRequest()
+                            .index("sensor") // 索引的名字是sensor
+                            .`type`("readingData")
+                            .source(json)
 
-        tableEnv
-                .connect(new FileSystem().path("F:\\workspace\\IDEA_Workspace\\FlinkSZ1128\\src\\main\\resources\\sensor.txt"))
-                .withFormat(new Csv()) // 按照csv文件格式解析文件
-                .withSchema( // 定义表结构
-                    new Schema()
-                            .field("id", DataTypes.STRING())
-                            .field("timestamp", DataTypes.BIGINT())
-                            .field("temperature", DataTypes.DOUBLE())
-                )
-                .createTemporaryTable("inputTable")    // 创建临时表
+                    requestIndexer.add(indexRequest)
+                }
+            }
+        )
 
-        val sensorTable: Table = tableEnv.from("inputTable") // 将临时表inputTable赋值到sensorTable
-        // 使用table api
-        val resultTable: Table = sensorTable
-                .select("id, temperature") // 查询`id`, `temperature` => (String, Double)
-                .filter("id = 'sensor_1'") // 过滤
+        // 用来定义每次写入多少条数据
+        // 成批的写入到es中去
+        esSinkBuilder.setBulkFlushMaxActions(10)
 
-        resultTable
-                .toAppendStream[(String, Double)] // 追加流
-                .print()
-
-        // 使用flink sql的方式查询
-        val resultSqlTable: Table = tableEnv
-                .sqlQuery("select id, temperature from inputTable where id ='sensor_1'")
-
-        resultSqlTable
-                .toAppendStream[(String, Double)] // 追加流
-                .print()
-
-        // 将DataStream转换成流表
-
-        val stream = env.addSource(new SensorSource)
-
-        val table = tableEnv.fromDataStream(stream, 'id, 'timestamp as 'ts, 'temperature as 'temp)
-        table
-                .select('id, 'temp)
-                .toAppendStream[(String, Double)]
-                .print()
+        stream.addSink(esSinkBuilder.build())
 
         env.execute()
     }
+
 }
